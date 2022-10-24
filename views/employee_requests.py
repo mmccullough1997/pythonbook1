55306d547723990ccb51eb17f685200a96587f0e
Mitch McCullough <mitchmccullough@gmail.com>
2022-10-23 19:42:31 -0500

    done

--- views/employee_requests.py
+++ views/employee_requests.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Employee
+from models import Employee, Location
 
 EMPLOYEES = [
     {
@@ -27,8 +27,12 @@ def get_all_employees():
             e.id,
             e.name,
             e.address,
-            e.location_id
+            e.location_id,
+            l.name location_name,
+            l.address location_address
         FROM employee e
+        JOIN Location l
+            ON l.id = e.location_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -46,6 +50,12 @@ def get_all_employees():
             # Animal class above.
             employee = Employee(row['id'], row['name'], row['address'],
                             row['location_id'])
+            
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            employee.location = location.__dict__
 
             employees.append(employee.__dict__)
 
