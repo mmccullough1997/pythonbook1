1458a4977457b27650762a847a512b3cabbc843d
Mitch McCullough <mitchmccullough@gmail.com>
2022-10-23 19:39:32 -0500

    added

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Animal
+from models import Animal, Location, Customer
 
 ANIMALS = [
     {
@@ -46,8 +46,18 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address,
+            c.name customer_name,
+            c.address customer_address,
+            c.email customer_email,
+            c.password customer_password
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
+        JOIN Customer c
+            ON c.id = a.customer_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -66,6 +76,18 @@ def get_all_animals():
             animal = Animal(row['id'], row['name'], row['breed'],
                             row['status'], row['location_id'],
                             row['customer_id'])
+            
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+            
+            # Create a Location instance from the current row
+            customer = Customer(row['id'], row['customer_name'], row['customer_address'],row['customer_email'],row['customer_password'])
+            
+            # Add the dictionary representation of the location to the animal
+            animal.customer = customer.__dict__
 
             animals.append(animal.__dict__)
 
